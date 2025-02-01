73c652b4af08c02d6541f26606de819796ed2d14
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-02-01 21:27:17 +0300

    Time: 0 ms (100%), Space: 17.8 MB (23.82%) - LeetHub

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -1,7 +1,6 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
         a = ''.join(map(str,digits))
-        result = []
         a = int(a)+1
         a = str(a)
         return list(map(int,a))
