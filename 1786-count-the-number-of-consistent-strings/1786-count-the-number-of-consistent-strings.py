c96dce47fd2c0188373d857b8ba651222d3d4a91
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-02-07 00:24:42 +0300

    Time: 222 ms (56.5%), Space: 19.8 MB (15.97%) - LeetHub

--- 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
+++ 1786-count-the-number-of-consistent-strings/1786-count-the-number-of-consistent-strings.py
@@ -3,7 +3,6 @@ class Solution:
         diA = set(allowed)
         count = 0
         for i in words:
-            diW = Counter(set(i))
             if len(set(i)-diA)==0:
                 count+=1
         return count
