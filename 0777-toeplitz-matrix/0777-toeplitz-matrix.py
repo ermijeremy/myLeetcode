750ae10119d15815a5d19b152fd07cbd40992110
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-02-06 18:42:27 +0300

    Time: 0 ms (100%), Space: 17.6 MB (81.42%) - LeetHub

--- 0777-toeplitz-matrix/0777-toeplitz-matrix.py
+++ 0777-toeplitz-matrix/0777-toeplitz-matrix.py
@@ -1,14 +1,7 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        ans = set()
-        for k in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                i,row = j,k
-                while i<len(matrix[0]) and row<len(matrix):
-                    ans.add(matrix[row][i])
-                    i+=1
-                    row+=1
-                if len(ans)!=1:
+        for row in range(len(matrix)-1):
+            for col in range(len(matrix[0])-1):
+                if matrix[row][col] != matrix[row+1][col+1]:
                     return False
-                ans = set()
         return True
