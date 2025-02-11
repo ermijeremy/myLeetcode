543fdefcc145562af4002ade5f88573e0961fc71
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-02-11 17:11:07 +0300

    Time: 3 ms (70.57%), Space: 17.6 MB (91.72%) - LeetHub

--- 1009-pancake-sorting/1009-pancake-sorting.py
+++ 1009-pancake-sorting/1009-pancake-sorting.py
@@ -1,12 +1,11 @@
 class Solution:
     def pancakeSort(self, arr: List[int]) -> List[int]:
         ans = []
-        mm = sorted(arr, reverse = True)
         n = len(arr)
         t = n
+
         for i in range(1,n):
-            j = i-1
-            m = mm[j]
+            m = max(arr[:n-i+1])
             if m != arr[n-i]:
                 k = arr.index(m)
                 ans.append(k+1)
@@ -14,4 +13,5 @@ class Solution:
                 ans.append(t)
                 arr[:n-i+1] = reversed(arr[:n-i+1])
             t-=1
+            
         return ans
