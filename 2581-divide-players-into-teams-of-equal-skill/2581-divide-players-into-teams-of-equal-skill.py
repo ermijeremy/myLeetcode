0fefd732ebb6ab8298acfad77ee1cbfa1bc1d994
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-02-16 00:11:17 +0300

    Time: 39 ms (76.92%), Space: 28.1 MB (89.69%) - LeetHub

---
+++ 2581-divide-players-into-teams-of-equal-skill/2581-divide-players-into-teams-of-equal-skill.py
@@ -0,0 +1,18 @@
+class Solution:
+    def dividePlayers(self, skill: List[int]) -> int:
+        skill.sort()
+        res = 0
+        n = len(skill)-1
+        temp_su = skill[0] + skill[-1]
+        
+        for left in range(len(skill)//2):
+
+            res += skill[left]*skill[n]
+            temp_sum = skill[left]+skill[n]
+            if temp_su != temp_sum:
+                return -1
+
+            temp_su = temp_sum
+            n -= 1
+
+        return res
