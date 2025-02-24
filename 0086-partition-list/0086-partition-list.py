0154b7f28210b5021d0eb770fa7cdcd8fd91f7b5
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-02-24 14:23:16 +0300

    Time: 0 ms (100%), Space: 18 MB (22.26%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -29,24 +29,3 @@ class Solution:
             return head
 
         return prev.next
-        # Definition for singly-linked list.
-# class Solution:
-#     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
-
-#         slist, blist = ListNode(), ListNode()
-#         small, big = slist, blist # dummy lists
-
-#         while head:
-#             if head.val < x:
-#                 small.next = head
-#                 small = small.next
-#             else:
-#                 big.next = head
-#                 big = big.next
-
-#             head = head.next
-
-#         small.next = blist.next
-#         big.next = None # prevent linked list circle
-
-#         return slist.next
