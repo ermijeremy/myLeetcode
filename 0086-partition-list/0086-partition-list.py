48d89d724141fffcfd241fda237f90d802a673ff
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-02-24 14:22:52 +0300

    Time: 0 ms (100%), Space: 18 MB (22.26%) - LeetHub

--- 0086-partition-list/0086-partition-list.py
+++ 0086-partition-list/0086-partition-list.py
@@ -1,52 +1,52 @@
-# class Solution:
-#     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:        
-        # if not head or not head.next:
-        #     return head
-        # # to target
-        # prev = ListNode()
-        # prev.next = head
-        # temp = prev
-        # target = target1 = None
-
-        # while temp:
-        #     if  temp.next and temp.val < x and temp.next.val >= x:
-        #         target = temp
-        #         target1 = temp.next
-        #         break
-        #     temp = temp.next
-
-        # if target and target1:
-        #     curr = target1
-        #     while curr:
-        #         while curr.next and curr.next.val < x:
-        #             temp = curr.next
-        #             target.next = temp
-        #             curr.next = temp.next
-        #             temp.next = target1
-        #             target = temp
-        #         curr = curr.next
-        # else:
-        #     return head
-
-        # return prev.next
-        # Definition for singly-linked list.
 class Solution:
-    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:        
+        if not head or not head.next:
+            return head
+        # to target
+        prev = ListNode(float("-inf"))
+        prev.next = head
+        temp = prev
+        target = target1 = None
 
-        slist, blist = ListNode(), ListNode()
-        small, big = slist, blist # dummy lists
+        while temp:
+            if  temp.next and temp.val < x and temp.next.val >= x:
+                target = temp
+                target1 = temp.next
+                break
+            temp = temp.next
 
-        while head:
-            if head.val < x:
-                small.next = head
-                small = small.next
-            else:
-                big.next = head
-                big = big.next
+        if target and target1:
+            curr = target1
+            while curr:
+                while curr.next and curr.next.val < x:
+                    temp = curr.next
+                    target.next = temp
+                    curr.next = temp.next
+                    temp.next = target1
+                    target = temp
+                curr = curr.next
+        else:
+            return head
 
-            head = head.next
+        return prev.next
+        # Definition for singly-linked list.
+# class Solution:
+#     def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
 
-        small.next = blist.next
-        big.next = None # prevent linked list circle
+#         slist, blist = ListNode(), ListNode()
+#         small, big = slist, blist # dummy lists
 
-        return slist.next
+#         while head:
+#             if head.val < x:
+#                 small.next = head
+#                 small = small.next
+#             else:
+#                 big.next = head
+#                 big = big.next
+
+#             head = head.next
+
+#         small.next = blist.next
+#         big.next = None # prevent linked list circle
+
+#         return slist.next
