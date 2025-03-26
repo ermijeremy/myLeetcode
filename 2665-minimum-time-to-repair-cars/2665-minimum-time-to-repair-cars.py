5e67342a94b227bd27ec864344d68bf7c5c4113d
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-03-26 17:59:36 +0300

    Time: 895 ms (39.23%), Space: 21.9 MB (84.05%) - LeetHub

--- 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
+++ 2665-minimum-time-to-repair-cars/2665-minimum-time-to-repair-cars.py
@@ -1,18 +1,18 @@
 class Solution:
     def repairCars(self, ranks: List[int], cars: int) -> int:
-        ranks.sort(reverse = True)
-
-        ans = float('inf')
-        left, right = 1, ranks[-1]*(cars**2)
-
-        while left <= right:
-            mid = (left+right)//2
-            t = 0
+        def valid(n):
+            ans = 0
             for i in ranks:
-                t += int(math.sqrt(mid/i))
-            if t >= cars:
-                ans = min(ans,mid)
-                right = mid-1
+                ans += int(math.sqrt(n/i))
+            return ans >= cars
+
+        l, r = 1, min(ranks)*(cars**2)
+
+        while l<=r:
+            mid = (l+r)//2
+
+            if valid(mid):
+                r = mid - 1
             else:
-                left = mid + 1
-        return ans
+                l = mid + 1
+        return l
