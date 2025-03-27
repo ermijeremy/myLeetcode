a8396bebdc824e77ea3b33d04f65d363eb8b2bed
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-03-27 17:35:51 +0300

    Time: 349 ms (61.84%), Space: 29.7 MB (47.96%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.py
@@ -1,20 +1,19 @@
 class Solution:
     def maximumCandies(self, candies: List[int], k: int) -> int:
-        if sum(candies)<k:
-            return 0
-        di = Counter(candies)
+        def valid(n):
+            count = 0
+            for i in candies:
+                count += i//n
+            return count>=k
 
-        def binary(arr, target, di):
-            ans, left, right = 0, 0, max(arr)
-            while left <= right:
-                mid = (left + right + 1)// 2
-                have = 0
-                for i,j in di.items():
-                    have += (i//mid)*(j)
-                if have >= target:
-                    ans = max(mid,ans)
-                    left = mid + 1
-                else:
-                    right = mid - 1
-            return ans
-        return binary(candies,k,di)
+        ans = 0
+        l, r = 1, max(candies)
+        while l<=r:
+            mid = (l+r)//2
+
+            if valid(mid):
+                l = mid + 1
+                ans = mid
+            else:
+                r = mid - 1
+        return ans
