e94f7c7aa4a45c06e48afdfd4b82b7a905fd5c2f
Ermias Kindalem <111380666+ermijeremy@users.noreply.github.com>
2025-04-26 00:02:54 +0300

    Time: 23 ms (34.62%), Space: 18 MB (47.46%) - LeetHub

--- 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
+++ 2856-count-complete-subarrays-in-an-array/2856-count-complete-subarrays-in-an-array.py
@@ -1,17 +1,40 @@
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        unique = set(nums)
-        n = len(unique)
-
-        window = []
-        ans = 0
+        k = len(set(nums))
+        ans1 = 0
         left = 0
+        window = defaultdict(int)
+        n = len(nums)
 
-        for i in range(len(nums)):
-            window = set()
-            for j in range(i,len(nums)):
-                window.add(nums[j])
-                if len(window)==n:
-                    ans += 1
-        
-        return ans
+        # getting the number of substrings which have at most k distnict elements
+
+        for right in range(n):
+            window[nums[right]] += 1
+            
+            while len(window) > k:
+                window[nums[left]] -= 1
+                if window[nums[left]] == 0:
+                    del window[nums[left]]
+                left += 1
+
+            ans1 += right - left + 1
+
+        ans2 = 0
+        left1 = 0
+        window1 = defaultdict(int)
+        n1 = len(nums)
+
+        # getting the number of substrings which have at most k-1 distinct elements
+
+        for right1 in range(n1):
+            window1[nums[right1]] += 1
+            
+            while len(window1) > k-1:
+                window1[nums[left1]] -= 1
+                if window1[nums[left1]] == 0:
+                    del window1[nums[left1]]
+                left1 += 1
+
+            ans2 += right1 - left1 + 1
+
+        return ans1 - ans2  # at most k distinct elements - at most k-1 ditnict elements == exactly k elements 
